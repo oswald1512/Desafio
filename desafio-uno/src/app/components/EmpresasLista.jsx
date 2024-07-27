284137c71b570e2e68ca326367373454fee5a818
oswald1512 <ht192210@alumno.udb.edu.sv>
2024-07-26 18:04:40 -0600

    Elaborando desafio

---
+++ desafio-uno/src/app/components/EmpresasLista.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {empresasMasValiosas} from "../data"; // Importa directamente el array con UUIDs
+
+export const EmpresasMasValiosasComponent = () => {
+    return (
+        <div className="container">
+        <div className="artistic-title">
+        <h1>Empresas Más Valiosas</h1>
+      </div>
+        <ul className="empresa-list">
+            {empresasMasValiosas.map((empresa) => (
+                <li key={empresa.uuid} className="empresa-item">
+                    <img src={empresa.imagen} alt={empresa.titulo} className="empresa-imagen" />
+                    <div className="empresa-content">
+                        <div className="empresa-info">
+                            <h2>{empresa.titulo}</h2>
+                            <p>{empresa.descripcion}</p>
+                            <p><strong>Características:</strong> {empresa.caracteristicas}</p>
+                        </div>          
+                    </div>
+                </li>
+            ))}
+        </ul>
+    </div>
+    );
+};
+
+export default EmpresasMasValiosasComponent;
+
+
+
---
+++ desafio-uno/src/app/page.js
@@ -0,0 +1,10 @@
+"use client"
+import { EmpresasMasValiosasComponent } from './components/EmpresasLista';
+
+export default function Home() {
+  return (
+    <>
+      <EmpresasMasValiosasComponent/>
+    </>
+  );
+}
