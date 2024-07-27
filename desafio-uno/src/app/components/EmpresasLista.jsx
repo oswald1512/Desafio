b3794b3949a50c115e6480ceedba719087a704f5
oswald1512 <ht192210@alumno.udb.edu.sv>
2024-07-27 10:26:56 -0600

    hola

--- desafio-uno/src/app/components/EmpresasLista.jsx
+++ desafio-uno/src/app/components/EmpresasLista.jsx
@@ -5,7 +5,7 @@ export const EmpresasMasValiosasComponent = () => {
     return (
         <div className="container">
         <div className="artistic-title">
-        <h1>Empresas Más Valiosas 1</h1>
+        <h1>Empresas Más Valiosas HOKN DSFUO 1</h1>
       </div>
         <ul className="empresa-list">
             {empresasMasValiosas.map((empresa) => (
